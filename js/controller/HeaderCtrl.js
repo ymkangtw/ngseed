8548e75b0eced1649c8dff90e6172749090fb29c
ymkangtw <ymkang.tw@yahoo.com.tw>
2015-11-23 18:20:42 +0800

    Demo Pages

---
+++ js/controller/HeaderCtrl.js
@@ -0,0 +1,13 @@
+﻿angular
+    .module('webapp')
+    .controller('HeaderCtrl', ['$mdSidenav', '$location', function($mdSidenav, $location) {
+        var self = this;
+        console.log('HeaderCtrl active');
+        self.OnLeftnavOpen = function() {
+        	$mdSidenav('leftnav').open();
+        };
+        self.OnLeftnavClose = function() {
+        	$mdSidenav('leftnav').close();
+        };
+
+    }]);
---
+++ js/controller/Option2Ctrl.js
@@ -0,0 +1,6 @@
+﻿angular
+    .module('webapp')
+    .controller('Option2Ctrl', ['$scope', function($scope) {
+        var self = this;
+
+    }]);
